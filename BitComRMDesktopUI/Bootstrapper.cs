cc54d6117baba1aff892d0a504d1e17f0e0843ee
iznogud112 <zare@live.co.uk>
2020-03-05 09:07:32 +0100

    Add WPF Project with MVVM
    Adds our WPF user interface project and configures with Caliburn Micro for our MVVM framework. Also includes the Simple Container DI system

---
+++ BitComRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,58 @@
+﻿using BitComRMDesktopUI.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BitComRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                            //service     //implementation
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel")).ToList() //ToList is IEnumerable
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    //service           //key                 //implementation
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
